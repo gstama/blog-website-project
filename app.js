47834a1fd491217cdfc3a972799228459f9e6ee3
gstama <gstama@outlook.com>
2021-02-05 22:10:44 +0000

    express route parameters work for posts and render new page with the chosen post

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const _ = require('lodash');
 
 const homeStartingContent =
   'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
@@ -48,6 +49,19 @@ app.post('/compose', (req, res, next) => {
   };
   posts.push(post);
   res.redirect('/');
+});
+
+app.get('/posts/:postName', (req, res, next) => {
+  posts.forEach((post) => {
+    if (_.lowerCase(req.params.postName) === _.lowerCase(post.title)) {
+      res.render('post', {
+        postTitle: post.title,
+        postBody: post.body,
+      });
+    } else {
+      console.log('Not a match!');
+    }
+  });
 });
 
 app.listen(3000, function () {
